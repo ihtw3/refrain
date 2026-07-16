d4cf4a4810deef62cb3c86b6ae0966d6051b8db6
ihtw3 <ljr486486@qq.com>
2026-07-16 15:47:51 +0800

    config 修改配置

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,10 +6,10 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "Refrain Blog",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "Refrain Blog",
 
 	// 站点 URL
 	site_url: "https://firefly.cuteleaf.cn",
