fcc4bef47c4172ea7f20f67cb88a53b4b6b7d3dd
ihtw3 <ljr486486@qq.com>
2026-07-20 17:30:52 +0800

    add年华

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -23,7 +23,7 @@ export const friendsPageConfig: FriendsPageConfig = {
 // 友链配置
 export const friendsConfig: FriendLink[] = [
 	{
-		title: "夏夜流萤",
+		title: "Refrain",
 		imgurl:
 			"https://weavatar.com/avatar/d252655d40d6874417a720bad0a6c5f77f8f6a1fd2f882f8f338402dc37e4190?s=640",
 		desc: "飞萤之火自无梦的长夜亮起，绽放在终竟的明天。",
@@ -50,6 +50,15 @@ export const friendsConfig: FriendLink[] = [
 		weight: 8,
 		enabled: true,
 	},
+	{
+		title: "年华",
+		imgurl: "https://q1.qlogo.cn/g?b=qq&nk=1323860289&s=640",
+		desc: "分享生活和技术。",
+		siteurl: "https://blog.amamo.top",
+		tags: ["Framework"],
+		weight: 8,
+		enabled: true,
+	}
 ];
 
 // 获取启用的友链并进行排序
